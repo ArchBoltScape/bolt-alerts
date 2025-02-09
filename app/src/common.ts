16f03b5a7af830390b1d4dc879f88b5c7319f242
Adam <classygopher@gmail.com>
2025-02-09 22:21:15 +0000

    format and minify

--- app/src/common.ts
+++ app/src/common.ts
@@ -1,16 +1,19 @@
-import { RuleType } from './interfaces';
+import { RuleType } from "./interfaces";
 
-export const getInitialAlertState = (ruletype: RuleType, hasnumber: boolean) => {
-    switch (ruletype) {
-        case RuleType.afktimer:
-        case RuleType.buff:
-        case RuleType.model:
-        case RuleType.stat:
-            return false;
-        case RuleType.chat:
-        case RuleType.popup:
-            return undefined;
-        case RuleType.xpgain:
-            return hasnumber ? false : undefined;
-    }
-}
+export const getInitialAlertState = (
+  ruletype: RuleType,
+  hasnumber: boolean,
+) => {
+  switch (ruletype) {
+    case RuleType.afktimer:
+    case RuleType.buff:
+    case RuleType.model:
+    case RuleType.stat:
+      return false;
+    case RuleType.chat:
+    case RuleType.popup:
+      return undefined;
+    case RuleType.xpgain:
+      return hasnumber ? false : undefined;
+  }
+};
